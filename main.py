3c0bb2a1247f059be9b53b9bd92fae0452ef00da
PizzaPiee <ullahmaaz3@gmail.com>
2021-04-05 15:16:58 +0200

    added tiles movement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pygame
-import os
+import pygame, os, sys, random
 
 pygame.init()
 pygame.display.init()
@@ -11,48 +10,67 @@ pygame.display.set_caption("Dango daikazoku")
 pygame.display.set_icon(ICON)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-
+clock = pygame.time.Clock()
 FPS = 60
+VEL = 3
 
 BACKGROUND = pygame.image.load(os.path.join('assets', 'background.png'))
 
+LANES = [1, WIDTH//4*1+2, WIDTH//4*2+2, WIDTH//4*3+2]
+DANGO_DAIKAZOKU = ['e5', 'd5', 'g5', 'g5', 'a5', 'a5', 'b5', 'g5']
+TILES = []
+top = 0
+for note in DANGO_DAIKAZOKU:
+    i = random.randint(0, 3)
+    tile = pygame.Rect(LANES[i], top, 100-2, 200)
+    top -= 205
+    TILES.append(tile)
+
 def draw_window():
+    WIN.fill((255, 255, 255))
+    WIN.blit(BACKGROUND, (0, 0))
+
     left = WIDTH//4
     while left<=WIDTH:
         border = pygame.Rect(left, 0, 2, HEIGHT)
         pygame.draw.rect(WIN, (255, 255, 255), border)
         left += WIDTH//4
 
-    pygame.display.update()
+    for tile in TILES:
+        pygame.draw.rect(WIN, (0, 0, 0), tile)
+
+    pygame.display.flip()
+
+def tiles_handle_movement():
+    for tile in TILES:
+        tile.y += VEL
 
 def main():
-    clock = pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(FPS)
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                sys.exit()
+
+        tiles_handle_movement()
+        clock.tick(FPS)
         draw_window()
-    pygame.quit()
 
 def start_menu():
-    clock = pygame.time.Clock()
     pygame.mixer.Sound.play(pygame.mixer.Sound(os.path.join('sounds', 'Shining_in_the_Sky.mp3')), loops=-1)
-    run = True
-    while run:
-        clock.tick(FPS)
-        WIN.blit(BACKGROUND, (0, 0))
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     pygame.mixer.stop()
                     main()
-                    pygame.display.quit()
             if event.type == pygame.QUIT:
-                run = False
+                pygame.quit()
+                sys.exit()
         
-        pygame.display.update()
+        WIN.blit(BACKGROUND, (0, 0))
+        clock.tick(FPS)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     start_menu()
